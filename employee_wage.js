ff2710ab708e84351f318a6d3eb455cfc5dc59ef
amitpanthi <apapanthi@gmail.com>
2021-09-15 17:57:00 +0530

    [Amit] Added UC7 - A class that wraps all Employee Wage functions

--- employee_wage.js
+++ employee_wage.js
@@ -12,6 +12,7 @@ var EmployeeWage = /** @class */ (function () {
     EmployeeWage.prototype.greet = function () {
         console.log("Hi " + this.name + ", Welcome to Employee Wage!");
     };
+    //UC1
     EmployeeWage.prototype.getAttendance = function () {
         var IS_ABSENT = 2;
         this.latestAttendance = Math.floor(Math.random() * 10) % 3; //0 or 1 or 2 
@@ -27,12 +28,15 @@ var EmployeeWage = /** @class */ (function () {
         }
         return currEmpAttendance;
     };
+    //UC2
     EmployeeWage.prototype.calcFullTime = function () {
         return FULL_TIME * WAGE_PER_HR;
     };
+    //UC3
     EmployeeWage.prototype.calcPartTime = function () {
         return PART_TIME * WAGE_PER_HR;
     };
+    //UC4
     EmployeeWage.prototype.getDailyPay = function () {
         var wage;
         var workShift = this.latestAttendance;
@@ -51,6 +55,7 @@ var EmployeeWage = /** @class */ (function () {
         }
         return wage;
     };
+    //UC5
     EmployeeWage.prototype.getMonthlyPay = function () {
         var todayWage = 0;
         var totalWage = 0;
@@ -84,6 +89,7 @@ var EmployeeWage = /** @class */ (function () {
         console.log("Days worked Part Time: " + partTimeCounter);
         console.log("Days absent: " + absentCounter);
     };
+    //UC6
     EmployeeWage.prototype.maxWorkingHours = function () {
         var totalWage = 0;
         var totalHours = 0;
@@ -124,164 +130,5 @@ var EmployeeWage = /** @class */ (function () {
         console.log("Days worked Part Time: " + partTimeCounter);
         console.log("Days absent: " + absentCounter);
     };
-    EmployeeWage.prototype.monthlyPayBreakdown = function () {
-        var totalWage = 0;
-        var totalHours = 0;
-        var todayHours = 0;
-        var fullTimeCounter = 0;
-        var partTimeCounter = 0;
-        var absentCounter = 0;
-        var wages = [];
-        var workHours = [];
-        for (var day = 1; day <= 20; day++) {
-            if (totalHours >= 160) {
-                break;
-            }
-            var currWorkShift = Math.floor(Math.random() * 10) % 3;
-            if (day == DAYS_IN_MONTH - 1) {
-                currWorkShift = this.latestAttendance;
-            }
-            switch (currWorkShift) {
-                case 1: // Full Time
-                    todayHours = FULL_TIME;
-                    fullTimeCounter += 1;
-                    break;
-                case 0: // Part Time
-                    todayHours = PART_TIME;
-                    partTimeCounter += 1;
-                    break;
-                default: //Absent
-                    todayHours = 0;
-                    absentCounter += 1;
-                    break;
-            }
-            workHours.push(todayHours);
-            wages.push(todayHours * WAGE_PER_HR);
-            totalHours += todayHours;
-        }
-        totalWage = totalHours * WAGE_PER_HR;
-        console.log("----------PER DAY BREAKDOWN MONTHLY PAY----------");
-        console.log("Per Day Hours worked: " + workHours.join(", "));
-        console.log("Per Day Wage earned: " + wages.join(", "));
-        console.log("Total Wage:" + totalWage);
-        console.log("Hours worked:" + totalHours);
-        console.log("Days passed: " + (day - 1));
-        console.log("Days worked Full Time: " + fullTimeCounter);
-        console.log("Days worked Part Time: " + partTimeCounter);
-        console.log("Days absent: " + absentCounter);
-        return [wages, workHours];
-    };
     return EmployeeWage;
 }());
-var employeeOne = new EmployeeWage("John");
-employeeOne.monthlyPayBreakdown();
-// // UC1 - Attendance Check
-// function attendanceCheck(): void {
-//     const IS_ABSENT = 0
-//     let currEmpAttendance = Math.floor(Math.random() * 10) % 2 //0 or 1 
-//     if (currEmpAttendance == IS_ABSENT){
-//         console.log("Employee is Absent")
-//     } else {
-//         console.log("Employee is Present")
-//     }
-// }
-// // UC2 - Daily Wage
-// function fullTimeWage(): number{
-//     return FULL_TIME * WAGE_PER_HR
-// }
-// // UC3 - Part Time Wage
-// function partTimeWage(): number{
-//     return PART_TIME * WAGE_PER_HR
-// }
-// // UC4 - Switch Case
-// let wage = 0
-// let workShift = Math.floor(Math.random() * 10) % 3
-// function getDailyPay(workShift: number): number{
-//     switch(workShift){
-//         case 1: // Full Time
-//             wage = WAGE_PER_HR * FULL_TIME
-//             console.log("Wage for full time: " + wage)
-//             break;
-//         case 0: // Part Time
-//             wage = WAGE_PER_HR * PART_TIME
-//             console.log("Wage for part time: " + wage)
-//             break;
-//         default: // Absent
-//             wage = 0
-//             console.log("Wage when absent: " + wage)
-//     }
-//     return wage
-// }
-// // UC5 - Monthly Wage
-// function getMonthlyWage(): void{
-//     let totalWage = 0
-//     let fullTimeCounter = 0
-//     let partTimeCounter = 0
-//     let absentCounter = 0
-//     const DAYS_IN_MONTH = 20
-//     let todayWage
-//     for(let day=0; day < DAYS_IN_MONTH; day++){
-//         let currWorkShift = Math.floor(Math.random() * 10) % 3
-//         switch(currWorkShift){
-//             case 1: // Full Time
-//                 todayWage = WAGE_PER_HR * FULL_TIME
-//                 fullTimeCounter += 1
-//                 break;
-//             case 0: // Part Time
-//                 todayWage = WAGE_PER_HR * PART_TIME
-//                 partTimeCounter += 1
-//                 break;
-//             default:
-//                 todayWage = 0
-//                 absentCounter += 1
-//                 break;
-//         }
-//         totalWage += todayWage
-//     }
-//     console.log("Total Wage:" + totalWage)
-//     console.log("Days worked Full Time: " + fullTimeCounter)
-//     console.log("Days worked Part Time: " + partTimeCounter)
-//     console.log("Days absent: " + absentCounter)
-// }
-// // UC6 - Max Working Hours
-// function maxWorkingHours(): void{
-//     let totalHours = 0
-//     let todayHours = 0
-//     let fullTimeCounter = 0
-//     let partTimeCounter = 0
-//     let absentCounter = 0
-//     let totalWage
-//     for(var day=1; day < Number.MAX_VALUE; day++){
-//         if(totalHours >= 160 || day >= 20){
-//             break;
-//         }
-//         let currWorkShift = Math.floor(Math.random() * 10) % 3
-//         switch(currWorkShift){
-//             case 1: // Full Time
-//                 todayHours = FULL_TIME
-//                 fullTimeCounter += 1
-//                 break;
-//             case 0: // Part Time
-//                 todayHours = PART_TIME
-//                 partTimeCounter += 1
-//                 break;
-//             default:
-//                 todayHours = 0
-//                 absentCounter += 1
-//                 break;
-//         }
-//         totalHours += todayHours
-//     }
-//     totalWage = totalHours * WAGE_PER_HR
-//     console.log("--------------------------------")
-//     console.log("Total Wage:" + totalWage)
-//     console.log("Hours worked:" + totalHours)
-//     console.log("Days passed: " + day)
-//     console.log("Days worked Full Time: " + fullTimeCounter)
-//     console.log("Days worked Part Time: " + partTimeCounter)
-//     console.log("Days absent: " + absentCounter)
-// }
-// // attendanceCheck()
-// // console.log(fullTimeWage())
-// // console.log(getDailyPay(workShift))
-// maxWorkingHours()
--- employee_wage.ts
+++ employee_wage.ts
@@ -45,5 +45,123 @@ class EmployeeWage{
     calcPartTime(): number{
         return PART_TIME * WAGE_PER_HR
     }
+
+    //UC4
+    getDailyPay(): number{
+        let wage
+        let workShift = this.latestAttendance
+
+        switch(workShift){
+            case 1: // Full Time
+                wage = WAGE_PER_HR * FULL_TIME
+                console.log("Wage for full time: " + wage)
+                break;
+            
+            case 0: // Part Time
+                wage = WAGE_PER_HR * PART_TIME
+                console.log("Wage for part time: " + wage)
+                break;
+            
+            default: // Absent
+                wage = 0
+                console.log("Wage when absent: " + wage)
+        }
+    
+        return wage
+    }
+
+    //UC5
+    getMonthlyPay(): void{
+        let todayWage = 0
+        let totalWage = 0
+        let fullTimeCounter = 0
+        let partTimeCounter = 0
+        let absentCounter = 0
+
+        for(let day=0; day < DAYS_IN_MONTH; day++){
+            let currWorkShift = Math.floor(Math.random() * 10) % 3
+        
+            if(day == DAYS_IN_MONTH - 1){
+                currWorkShift = this.latestAttendance
+            }
+
+            switch(currWorkShift){
+                case 1: // Full Time
+                    todayWage = WAGE_PER_HR * FULL_TIME
+                    fullTimeCounter += 1
+                    break;
+                
+                case 0: // Part Time
+                    todayWage = WAGE_PER_HR * PART_TIME
+                    partTimeCounter += 1
+                    break;
+                
+                default: // Absent
+                    todayWage = 0
+                    absentCounter += 1
+                    break;
+            }
+        
+            totalWage += todayWage
+        }
+        
+        console.log("----------MONTHLY PAY----------")
+        console.log("Total Wage:" + totalWage)
+        console.log("Days worked Full Time: " + fullTimeCounter)
+        console.log("Days worked Part Time: " + partTimeCounter)
+        console.log("Days absent: " + absentCounter)
+    }
+
+    //UC6
+    maxWorkingHours(): void{
+        let totalWage = 0
+        let totalHours = 0
+        let todayHours = 0
+        let fullTimeCounter = 0
+        let partTimeCounter = 0
+        let absentCounter = 0
+
+        for(var day=1; day <= 20; day++){
+            if(totalHours >= 160){
+                break;
+            }
+
+            let currWorkShift = Math.floor(Math.random() * 10) % 3
+            if(day == DAYS_IN_MONTH - 1){
+                currWorkShift = this.latestAttendance
+            }
+
+            switch(currWorkShift){
+                case 1: // Full Time
+                    todayHours = FULL_TIME
+                    fullTimeCounter += 1
+                    break;
+                
+                case 0: // Part Time
+                    todayHours = PART_TIME
+                    partTimeCounter += 1
+                    break;
+                
+                default: //Absent
+                    todayHours = 0
+                    absentCounter += 1
+                    break;
+            }
+
+            totalHours += todayHours
+        }
+
+        totalWage = totalHours * WAGE_PER_HR
+
+        console.log("----------CONDITION BASED MONTHLY PAY----------")
+        console.log("Total Wage:" + totalWage)
+        console.log("Hours worked:" + totalHours)
+        console.log("Days passed: " + (day - 1))
+        console.log("Days worked Full Time: " + fullTimeCounter)
+        console.log("Days worked Part Time: " + partTimeCounter)
+        console.log("Days absent: " + absentCounter)
+    }
+
+    
 }
 
